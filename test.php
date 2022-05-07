c3c008a007b7437e63c510466889fa65b792708e
isaac amoafo <amoafoi12@gmail.com>
2022-05-07 19:25:08 +0000

    added time and date

--- test.php
+++ test.php
@@ -6,6 +6,9 @@ $table = "todo_list";
 
 try {
     $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
+    echo " <h1 style=color:red> ". date('l') ."</h1> ";
+    echo "time: ".date("h:i:sa") . "<br>";
+    echo "date: ".date("Y-m-d");
     echo "<h2>TODO</h2><ol>"; 
     foreach($db->query("SELECT content FROM $table") as $row) {
       echo "<li>" . $row['content'] . "</li>";
@@ -16,3 +19,5 @@ try {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
 }
+
+?>
